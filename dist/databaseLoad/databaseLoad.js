dca6f96ecbd10b551f75c179bf624f3058745e60
EmilAvramov <e_avramov@zoho.eu>
2022-09-15 14:03:57 +0300

    more adjustments for beanstalk deployment

--- dist/databaseLoad/databaseLoad.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const loadService_1 = require("./loadService");
-(0, loadService_1.getBrands)();
-(0, loadService_1.cleanData)();
--- dist/interfaces/Brand.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Brand = void 0;
-const sequelize_1 = require("sequelize");
-class Brand extends sequelize_1.Model {
-}
-exports.Brand = Brand;
